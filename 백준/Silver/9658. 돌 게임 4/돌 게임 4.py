61528b439f28b870e17a61b21096e57a7fa799eb
protein00 <71213378+protein00@users.noreply.github.com>
2023-08-02 13:39:15 +0900

    [Silver II] Title: 돌 게임 4, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.py
@@ -0,0 +1,18 @@
+N = int(input())
+dp = [-1] * 1001
+dp[1] = 0
+dp[2] = 1
+dp[3] = 0
+dp[4] = 1
+dp[5] = 1
+
+for i in range(6, N+1):
+    if dp[i-1] == 0 or dp[i-3] == 0 or dp[i-4] == 0:
+        dp[i] = 1
+    else:
+        dp[i] = 0
+
+if dp[N] == 1:
+    print("SK")
+else:
+    print("CY")

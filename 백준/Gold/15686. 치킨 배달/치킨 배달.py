c9a77d3edc5ee5fac50c41a9fcb68d802bef3bfc
protein24g <71213378+protein24g@users.noreply.github.com>
2023-08-16 10:26:51 +0900

    [Gold V] Title: 치킨 배달, Time: 628 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+ary = []
+home, chik = [], []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for j, k in enumerate(tmp):
+        if k == 1:
+            home.append([i, j])
+        elif k == 2:
+            chik.append([i, j])
+
+res = 1e9
+for ch in combinations(chik, m):
+    com_res = 0
+    for h in home:
+        dist = 1e9
+        for c in ch:
+            dist = min(dist, abs(h[0] - c[0]) + abs(h[1] - c[1]))
+        com_res += dist
+    res = min(res, com_res)
+print(res)

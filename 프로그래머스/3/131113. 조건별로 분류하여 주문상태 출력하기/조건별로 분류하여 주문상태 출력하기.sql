7742ac58bccc9dbab8483d2cba14c173088d8002
protein24g <71213378+protein24g@users.noreply.github.com>
2023-10-23 13:15:46 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,7 @@
+SELECT ORDER_ID,
+    PRODUCT_ID,
+    DATE_FORMAT(OUT_DATE, "%Y-%m-%d") OUT_DATE,
+    IF (OUT_DATE IS NULL, "출고미정",
+       IF (DATEDIFF(DATE_FORMAT(OUT_DATE, "%Y-%m-%d"), "2022-05-01") > 0, "출고대기", "출고완료")) 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
